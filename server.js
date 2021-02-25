52e7c61609d6196156421eca07a0d84b4ae4a712
Merlin Moelter <joebau.0815@googlemail.com>
2021-02-25 17:50:40 +0100

    Add server for production

---
+++ server.js
@@ -0,0 +1,19 @@
+const fastify = require("fastify")()
+const static = require("fastify-static")
+const path = require("path")
+require("dotenv").config()
+
+const BUILD_DIR = path.join(__dirname, "build")
+
+fastify.register(static, {
+    root: BUILD_DIR,
+    wildcard: false
+})
+
+fastify.get("/*", (_req, reply) => {
+    reply.sendFile("index.html")
+})
+
+fastify.listen(process.env.PORT).then(() => {
+    console.log(`Serving frontend on port ${process.env.PORT}`)
+})
