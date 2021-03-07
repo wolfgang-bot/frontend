0cdfdd92b9022a5bdc0344515170a672ab9d4f1c
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-07 12:11:35 +0100

    Remove commands from index page

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -3,15 +3,12 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import Title from "../components/Styled/Title"
 import ModuleList from "../features/modules/ModuleList"
-import CommandListForModule from "../features/commands/CommandListForModule"
 
 function IndexPage() {
     return (
         <Layout>
             <Title>Modules</Title>
             <ModuleList/>
-            <Title>Commands</Title>
-            <CommandListForModule moduleKey="main"/>
         </Layout>
     )
 }
