7831e1be4d219047bccc25efd1efdf743b373b7b
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-07 17:55:51 +0100

    Use env variable for discord oauth url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,7 +8,9 @@ export const API_TOKEN = localStorage.getItem(API_TOKEN_STORAGE_KEY)
 
 export const DISCORD_OAUTH_API_ENDPOINT = API_BASE_URL + "/oauth/discord"
 export const DISCORD_OAUTH_URL =
-    `https://discord.com/api/oauth2/authorize?client_id=772865751560093708&redirect_uri=${encodeURIComponent(
+    `https://discord.com/api/oauth2/authorize?client_id=${
+        process.env.REACT_APP_DISCORD_BOT_CLIENT_ID
+    }&redirect_uri=${encodeURIComponent(
         DISCORD_OAUTH_API_ENDPOINT
     )}&response_type=code&scope=identify%20guilds`
 export const DISCORD_CDN_BASE_URL = "https://cdn.discordapp.com"
