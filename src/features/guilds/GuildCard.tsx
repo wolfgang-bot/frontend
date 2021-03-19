b834bf7e064817b2454311b56f7ba6de9833f87d
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-19 15:07:54 +0100

    Add active guild indicator

--- src/features/guilds/GuildCard.tsx
+++ src/features/guilds/GuildCard.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react"
 import { useHistory } from "react-router-dom"
-import { Card, CardActionArea, CardHeader, Typography } from "@material-ui/core"
+import { Box, Card, CardActionArea, CardHeader, Typography } from "@material-ui/core"
 import Skeleton from "@material-ui/lab/Skeleton"
+import InactiveIcon from "@material-ui/icons/Close"
+import ActiveIcon from "@material-ui/icons/Check"
 
 import { API } from "../../config/types"
 import GuildIcon from "../../components/Discord/GuildIcon"
@@ -35,20 +37,34 @@ function GuildCard({ guild, className }: {
             onMouseLeave={() => setIsMouseOver(false)}
         >
             <CardActionArea onClick={handleClick}>
-                <CardHeader
-                    disableTypography
-                    avatar={
-                        <GuildIcon
-                            guild={guild}
-                            animated={isMouseOver}
-                        />
-                    }
-                    title={
-                        <Typography variant="body1">
-                            {guild.name}
-                        </Typography>
-                    }
-                />
+                <Box
+                    display="flex"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <CardHeader
+                        disableTypography
+                        avatar={
+                            <GuildIcon
+                                guild={guild}
+                                animated={isMouseOver}
+                            />
+                        }
+                        title={
+                            <Typography variant="body1">
+                                {guild.name}
+                            </Typography>
+                        }
+                    />
+
+                    <Box mr={2}>
+                        {
+                            !guild.isActive ?
+                            <InactiveIcon/> :
+                            <ActiveIcon/>
+                        }
+                    </Box>
+                </Box>
             </CardActionArea>
         </Card>
     )
