cde532ff1c14cee643f328ce67e4be1bfde6d929
Tracer1337 <merlin.moelter@gmail.com>
2021-04-27 19:31:59 +0200

    Fix settings dialog crash

--- src/components/Dialogs/GuildSettingsDialog.tsx
+++ src/components/Dialogs/GuildSettingsDialog.tsx
@@ -1,6 +1,6 @@
-import React, { useRef } from "react"
+import React, { useEffect, useRef } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@material-ui/core"
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, CircularProgress } from "@material-ui/core"
 
 import { API } from "../../config/types"
 import { RootState } from "../../store"
@@ -9,6 +9,8 @@ import { updateConfig as updateConfigAction } from "../../features/moduleInstanc
 import Logger from "../../utils/Logger"
 import opener from "../ComponentOpener"
 import api from "../../api"
+import withStreamSubscription from "../../features/streams/withStreamSubscription"
+import { fetchModules } from "../../features/modules/modulesSlice"
 
 function GuildSettingsDialog({ open, onClose, guild }: {
     open: boolean,
@@ -18,6 +20,8 @@ function GuildSettingsDialog({ open, onClose, guild }: {
     const dispatch = useDispatch()
     
     const module = useSelector((store: RootState) => store.modules.data.settings)
+    const status = useSelector((store: RootState) => store.modules.status)
+    const error = useSelector((store: RootState) => store.modules.error)
     const instance = useSelector((store: RootState) => store.moduleInstances.guilds[guild.id]?.data.settings)
 
     const argsFormRef = useRef<RefHandle>(null)
@@ -72,29 +76,49 @@ function GuildSettingsDialog({ open, onClose, guild }: {
         }
     }
 
-    return (
-        <Dialog open={open} onClose={onClose}>
-            <DialogTitle>Settings</DialogTitle>
+    useEffect(() => {
+        if (status === "idle") {
+            dispatch(fetchModules("ws"))
+        }
+    }, [status])
 
-            <DialogContent>
-                <ArgumentsForm
-                    args={module.args}
-                    guild={guild}
-                    currentConfig={instance.config}
-                    ref={argsFormRef}
-                />
-            </DialogContent>
+    if (status === "success") {
+        return (
+            <Dialog open={open} onClose={onClose}>
+                <DialogTitle>Settings</DialogTitle>
+    
+                <DialogContent>
+                    <ArgumentsForm
+                        args={module.args}
+                        guild={guild}
+                        currentConfig={instance.config}
+                        ref={argsFormRef}
+                    />
+                </DialogContent>
+    
+                <DialogActions>
+                    <Button
+                        onClick={handleSubmit}
+                        color="primary"
+                    >
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
 
-            <DialogActions>
-                <Button
-                    onClick={handleSubmit}
-                    color="primary"
-                >
-                    Save
-                </Button>
-            </DialogActions>
-        </Dialog>
-    )
+    if (status === "error") {
+        return <div>{error}</div>
+    }
+
+    return <CircularProgress/>
 }
 
-export default GuildSettingsDialog
+export default withStreamSubscription(
+    GuildSettingsDialog,
+    "guild-module-instances",
+    {
+        showOverlayIfEmpty: false
+    }
+)
