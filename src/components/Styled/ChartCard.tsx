69e3388566ffda00fb5954aa06d0b9c074916ed7
Tracer1337 <merlin.moelter@gmail.com>
2021-04-27 21:50:20 +0200

    Add fullscreen mode to charts

--- src/components/Styled/ChartCard.tsx
+++ src/components/Styled/ChartCard.tsx
@@ -1,36 +1,73 @@
 import React, { useState } from "react"
-import { Paper, Box, Typography, Divider, IconButton } from "@material-ui/core"
+import { Paper, Box, Typography, Divider, IconButton, Dialog } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
 import MagnetOnIcon from "@material-ui/icons/GridOn"
 import MagnetOffIcon from "@material-ui/icons/GridOff"
+import FullscreenIcon from "@material-ui/icons/Fullscreen"
+import CloseIcon from "@material-ui/icons/Close"
 
-function ChartCard({ chart, label, className, width = "100%" }: {
+const DIALOG_HEADER_HEIGHT = 56
+
+const useStyles = makeStyles({
+    fullscreenIcon: {
+        fontSize: 26
+    }
+})
+
+type ContentProps = {
     chart: React.ReactElement,
     label: React.ReactNode,
-    width?: number | string | null,
-    className?: string
-}) {
-    const [hasMagnetCursor, setHasMagnetCursor] = useState<boolean>(true)
+    isFullscreen: boolean,
+    onToggleFullscreen: () => void
+}
+
+function Content({
+    chart,
+    label,
+    isFullscreen,
+    onToggleFullscreen
+}: ContentProps) {
+    const classes = useStyles()
+
+    const [hasMagnetCursor, setHasMagnetCursor] = useState(true)
 
     const toggleHasMagnetCursor = () => {
         setHasMagnetCursor(!hasMagnetCursor)
     }
 
     return (
-        <Box width={width}>
-            <Paper variant="outlined" className={className}>
+        <>
+            <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Box ml={3} my={2}>
+                    <Typography variant="body1">{label}</Typography>
+                </Box>
+
                 <Box
                     display="flex"
-                    justifyContent="space-between"
                     alignItems="center"
+                    mr={1}
                 >
-                    <Box mx={3} my={2}>
-                        <Typography variant="body1">{label}</Typography>
-                    </Box>
+                    {!isFullscreen && (
+                        <Box mr={1}>
+                            <IconButton
+                                size="small"
+                                onClick={onToggleFullscreen}
+                            >
+                                <FullscreenIcon
+                                    className={classes.fullscreenIcon}
+                                />
+                            </IconButton>
+                        </Box>
+                    )}
 
-                    <Box mx={2}>
+                    <Box mr={1}>
                         <IconButton
                             size="small"
-                        onClick={toggleHasMagnetCursor}
+                            onClick={toggleHasMagnetCursor}
                         >
                             {
                                 hasMagnetCursor ?
@@ -39,17 +76,72 @@ function ChartCard({ chart, label, className, width = "100%" }: {
                             }
                         </IconButton>
                     </Box>
-                </Box>
 
-                <Divider/>
-
-                <Box mt={2}>
-                    {React.cloneElement(React.Children.only(chart), {
-                        hasMagnetCursor
-                    })}
+                    {isFullscreen && (
+                        <Box mr={1}>
+                            <IconButton
+                                size="small"
+                                onClick={onToggleFullscreen}
+                            >
+                                <CloseIcon/>
+                            </IconButton>
+                        </Box>
+                    )}
                 </Box>
-            </Paper>
-        </Box>
+            </Box>
+
+            <Divider/>
+
+            <Box mt={2}>
+                {React.cloneElement(React.Children.only(chart), {
+                    hasMagnetCursor,
+                    ...(isFullscreen ? {
+                        height: (window.innerHeight - DIALOG_HEADER_HEIGHT) * .9
+                    } : {})
+                })}
+            </Box>
+        </>
+    )
+}
+
+type Props = {
+    width?: number | string | null,
+    className?: string,
+    chart: ContentProps["chart"],
+    label: ContentProps["label"]
+}
+
+function ChartCard({ className, width = "100%", ...props }: Props) {
+    const [isFullscreen, setIsFullscreen] = useState(false)
+
+    const toggleFullscreen = () => {
+        setIsFullscreen(!isFullscreen)
+    }
+
+    return (
+        <>
+            <Box width={width}>
+                <Paper variant="outlined" className={className}>
+                <Content
+                    onToggleFullscreen={toggleFullscreen}
+                    isFullscreen={false}
+                    {...props}
+                />
+                </Paper>
+            </Box>
+            
+            <Dialog
+                open={isFullscreen}
+                onClose={toggleFullscreen}
+                fullScreen
+            >
+                <Content
+                    onToggleFullscreen={toggleFullscreen}
+                    isFullscreen={true}
+                    {...props}
+                />
+            </Dialog>
+        </>
     )
 }
 
