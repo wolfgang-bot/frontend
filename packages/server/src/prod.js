c22ecf3e4860b35034c317a16b080f9d2324c246
Tracer1337 <merlin.moelter@gmail.com>
2021-05-11 19:03:31 +0200

    Fix broken dashboard rotes

--- packages/server/src/prod.js
+++ packages/server/src/prod.js
@@ -7,14 +7,22 @@ const DASHBOARD_BUILD_DIR = path.join(PACKAGES_DIR, "dashboard", "build")
 const STATIC_BUILD_DIR = path.join(PACKAGES_DIR, "static", "build")
 
 fastify.register(static, {
-    root: STATIC_BUILD_DIR
+    root: STATIC_BUILD_DIR,
+    decorateReply: false
 })
 
 fastify.register(static, {
     root: DASHBOARD_BUILD_DIR,
     prefix: "/dashboard",
-    decorateReply: false,
-    prefixAvoidTrailingSlash: true
+    wildcard: false
+})
+
+fastify.get("/dashboard/*", (_req, reply) => {
+    reply.sendFile("index.html")
+})
+
+fastify.get("/dashboard", (_req, reply) => {
+    reply.sendFile("index.html")
 })
 
 fastify.listen(process.env.PORT, process.env.HOST).then(() => {
